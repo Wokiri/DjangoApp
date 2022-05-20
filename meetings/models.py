dc9e55361a6e3920406b7b37be8aa2d6866ccb4e
Joe Oketch <wokirijoe@gmail.com>
2022-05-20 16:10:05 +0300

    Changed models

--- meetings/models.py
+++ meetings/models.py
@@ -1,10 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
 from django.core.exceptions import ValidationError
 from django.urls import reverse
-
-from .manager import CustomUserManager
 
 import uuid
 
@@ -21,22 +19,14 @@ class ROLE(models.TextChoices):
     MEMBER = 'Member', _('Member')
 
 
-class OrganizationMember(AbstractUser):
+class OrganizationMember(models.Model):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
-    role = models.CharField(max_length=11, choices=ROLE.choices, default=ROLE.MEMBER)
     gender = models.CharField(max_length=6, choices=GENDER.choices)
-    thumbnail = models.ImageField(upload_to='thumbnails/', default='default_avatar.png') # Img in media
     date_joined = models.DateField(auto_now_add=True)
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
-
-    username = None
-
-    objects = CustomUserManager()
+    
 
     class Meta:
         verbose_name = 'Organization member'
@@ -58,7 +48,8 @@ class OrganizationMember(AbstractUser):
 
 class Meeting(models.Model):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
-    meeting_title = models.CharField(max_length=255)
+    meeting_title = models.CharField(max_length=125)
+    short_description = models.CharField(max_length=255)
     date = models.DateField(auto_now_add=True)
     start_time = models.DateTimeField(null=True, blank=True)
     end_time = models.DateTimeField(null=True, blank=True)
@@ -101,6 +92,6 @@ class Agenda(models.Model):
         return self.title
 
     class Meta:
-        verbose_name = 'Agendanum'
+        verbose_name = 'Agendum'
         verbose_name_plural = 'Agenda'
         ordering = ['-created']
